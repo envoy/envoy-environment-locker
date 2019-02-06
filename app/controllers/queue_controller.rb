e410a90d4a4104daee26406ed78749a401df5039
sammilechman <sammilechman@gmail.com>
2019-02-06 12:05:51 -0800

    Add validation of slack secret token

--- app/controllers/queue_controller.rb
+++ app/controllers/queue_controller.rb
@@ -2,10 +2,20 @@ class QueueController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def perform_action
-    render json: QueueManager.new(user_id).perform(command)
+    begin
+      validate_token
+      render json: QueueManager.new(user_id).perform(command)
+    rescue => error
+      Rails.logger.info(error.message)
+      render json: { text: "There was a problem with the request" }, status: 400
+    end
   end
 
   private
+
+  def validate_token
+    raise "Invalid token" if token != ENV["SLACK_SECRET_TOKEN"]
+  end
 
   def user_id
     params[:user_id]
@@ -13,5 +23,9 @@ class QueueController < ApplicationController
 
   def command
     params[:command]
+  end
+
+  def token
+    params[:token]
   end
 end
--- spec/controllers/queue_controller_spec.rb
+++ spec/controllers/queue_controller_spec.rb
@@ -1,20 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe QueueController, type: :controller do
+  before do
+    stub_env("SLACK_SECRET_TOKEN", "abcdefg")
+  end
+
   describe ".perform_action" do
     subject { post :perform_action, params: params }
 
     let(:params) do
       {
-        user_id: user_id
+        user_id: user_id,
+        command: command,
+        token: token
       }
     end
     let(:user_id) { "blah" }
+    let(:command) { "/lock" }
 
-    it "returns a 200" do
-      subject
-      
-      expect(response.code).to eq("200")
+    describe "with a good token" do
+      let(:token) { "abcdefg" }
+
+      it "returns a 200" do
+        subject
+
+        expect(response.code).to eq("200")
+      end
+    end
+
+    describe "with a bad token" do
+      let(:token) { "zzzzzzzz" }
+
+      it "informs the client that there was an issue with the request" do
+        subject
+
+        expect(response.code).to eq("400")
+        expect(JSON.parse(response.body)["text"]).to eq("There was a problem with the request")
+      end
     end
   end
 end
