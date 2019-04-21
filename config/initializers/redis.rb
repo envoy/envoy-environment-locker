736a29dc4d9dfa53969c0b14202713749965c971
Nicolás Hock Isaza <nhocki@gmail.com>
2019-04-20 21:59:56 -0500

    Don't run autoexpire automatically
    This commit prevents the autoexpire feature to run automatically in a
    new thread. This is because this only works when there is exactly one
    instance of the app running.

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,9 +2,11 @@ require "thread"
 
 REDIS = Redis.new(url: ENV.fetch("REDIS_URL", "redis://localhost:6379"))
 
-t = Thread.new do
-  while true
-    AutoExpirer.expire_all!
-    sleep(60) # Sleep for 1 minute
+if ActiveModel::Type::Boolean.new.cast(ENV["THREADED_AUTO_EXPIRE"])
+  Thread.new do
+    while true
+      AutoExpirer.expire_all!
+      sleep(60) # Sleep for 1 minute
+    end
   end
 end
